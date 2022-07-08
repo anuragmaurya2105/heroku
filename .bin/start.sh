ed0d212ebece97d429336b4a02f5c758a19775ec
Anupam Maurya <83022538+anuragmaurya2105@users.noreply.github.com>
2022-07-08 22:52:25 +0530

    Update start.sh

--- .bin/start.sh
+++ .bin/start.sh
@@ -14,9 +14,9 @@ if [ "${LIBDRIVE_VERSION}" != "dev" ]; then
     fi
 
     if [ ! -z "${LIBDRIVE_REPOSITRY}" ]; then
-        REPO="anuragmaurya2105/libDrive"
+        REPO=${LIBDRIVE_REPOSITRY}
     else
-        REPO="anuragmaurya2105/libDrive"
+        REPO="libDrive/libDrive"
     fi
 
     curl -L -s $(curl -s "https://api.github.com/repos/${REPO}/releases/${VER}" | grep -Po '"browser_download_url": "\K.*?(?=")') | tar xf - -C .
