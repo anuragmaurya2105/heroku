03d3528bce9e8349764194b4563365a8f0038542
Anupam Maurya <83022538+anuragmaurya2105@users.noreply.github.com>
2022-07-08 21:55:31 +0530

    Update setup.sh

--- .bin/setup.sh
+++ .bin/setup.sh
@@ -6,7 +6,7 @@ mkdir ./dev
 cd ./dev
 mkdir ./tmp
 cd ./tmp
-git clone "https://github.com/libDrive/server.git" --depth 1 server
+git clone "https://github.com/anuragmaurya2105/server" --depth 1 server
 git clone "https://github.com/anuragmaurya2105/web" --depth 1 web
 mkdir ./libDrive.Server
 mkdir ./libDrive.Server/build
